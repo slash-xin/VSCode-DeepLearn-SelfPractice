b34f0cfa370314b7ed83d898a940ab5c92260d8e
Slash.Xin <slash.xin@gmail.com>
2018-03-01 22:56:30 +0800

    updated at ASUS

--- Course1-3/SelfLearning_DeepNeuralNetwork.py
+++ Course1-3/SelfLearning_DeepNeuralNetwork.py
@@ -399,6 +399,41 @@ print('W2 =', parameters['W2'])
 print('b2 =', parameters['b2'])
 
 
+# ---------------------------------
+# 6. Use Model to Predict 
+# ---------------------------------
+def predict(X, Y, parameters):
+    '''
+    This function is used to predict the results of a L-layer neural network.
+
+    Arguments:
+    X -- data set of examples will be predicted
+    parameters -- parameters of the trained model
+
+    Returns:
+    p -- predictions for the given dataset X
+    '''
+    m = X.shape[1]
+    n = len(parameters) // 2
+    p = np.zeros((1, m))
+
+    probas, caches = L_model_forward(X, parameters)
+
+    for i in range(0, probas.shape[1]):
+        if probas[0, i] > 0.5:
+            p[0, i] = 1
+        else:
+            p[0, i] = 0
+    
+    print('Accuracy:', np.sum((p == Y)/m))
+
+    return p
+
+
+
+
+
+
 
 
 
@@ -445,7 +480,7 @@ def load_data(path):
     
     return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
-train_x_orig, train_y, test_x_orig, test_y, classes =load_data('D:/JupyterNotebook/DeepLearning/deeplearning.ai-master/course1/assignment1-4/assignment4/datasets')
+train_x_orig, train_y, test_x_orig, test_y, classes =load_data('course1-3/datasets')
 
 # Show a picture
 index = 8
@@ -465,3 +500,149 @@ print("train_x_orig shape: ", train_x_orig.shape)
 print("train_y shape: ", train_y.shape)
 print("test_x_orig shape: ", test_x_orig.shape)
 print("test_y shape: ", test_y.shape)
+
+# Reshape the training and test data
+train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T
+test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+
+# Standardize data to have feature values between 0 and 1.
+train_x = train_x_flatten / 255.
+test_x = test_x_flatten / 255.
+
+print('shape of train_x:', train_x.shape)
+print('shape of test_x:', test_x.shape)
+
+# Architecture a 2-layer neural network model
+# CONSTANTS DEFINITION THE MODEL
+n_x = 12288
+n_h = 7
+n_y = 1
+layer_dims = (n_x, n_h, n_y)
+
+# two layer model
+def two_layer_model(X, Y, layer_dims, learning_rate=0.0075, num_iterations=3000, print_cost=False):
+    '''
+    Implement a two-layer neural network: LINEAR -> RELU -> LINEAR -> SIGMOID
+
+    Arguments:
+    X -- input data, of shape (n_x, number of examples)
+    Y -- true 'label' vector (containing 0 if not-cat; 1 if cat) of shape (1, number of examples)
+    layer_dims -- dimensions of the layers (n_x, n_h, n_y)
+    learning_rate -- learning rate of the gradient descent update rule
+    num_iterations -- number of iterations of the optimization loop
+    print_cost -- If set to True, this will print the cost every 100 iterations
+
+    Returns:
+    parameters -- a dictionary containing W1, W2, b1 and b2
+    '''
+    np.random.seed(1)
+    grads = {}
+    costs = []
+    m = X.shape[1]
+    (n_x, n_h, n_y) = layer_dims
+
+    # Initialize the parameters
+    parameters = initialize_parameters(n_x, n_h, n_y)
+
+    # Get W1, b1, W2, b2 from the dictionary parameters
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+
+    for i in range(0, num_iterations):
+        # Forward propagation
+        A1, cache1 = linear_activation_forward(X, W1, b1, 'relu')
+        A2, cache2 = linear_activation_forward(A1, W2, b2, 'sigmoid')
+
+        # Compute cost
+        cost = compute_cost(A2, Y)
+
+        dA2 = -(np.divide(Y, A2) - np.divide(1-Y, 1-A2))
+
+        # Backward propagation
+        dA1, dW2, db2 = linear_activation_backward(dA2, cache2, 'sigmoid')
+        dA0, dW1, db1 = linear_activation_backward(dA1, cache1, 'relu')
+
+        grads['dW1'] = dW1
+        grads['db1'] = db1
+        grads['dW2'] = dW2
+        grads['db2'] = db2
+
+        # Update parameters
+        parameters = update_parameters(parameters, grads, learning_rate)
+
+        # Retrieve W1, b1, W2, b2 from parameters
+        W1 = parameters['W1']
+        b1 = parameters['b1']
+        W2 = parameters['W2']
+        b2 = parameters['b2']
+
+        if i % 100 == 0:
+            costs.append(np.squeeze(cost))
+            if print_cost:
+                print('Cost after iteration {0}: {1}'.format(i, np.squeeze(cost)))
+    
+    plt.plot(costs)
+    plt.ylabel('Cost')
+    plt.xlabel('iterations (per tens)')
+    plt.title('Learning rate=' + str(learning_rate))
+    plt.show()
+
+    return parameters
+
+#parameters = two_layer_model(train_x, train_y, layer_dims, num_iterations=2500, print_cost=True)
+
+# predict
+#predictions_train = predict(train_x, train_y, parameters)
+#predictions_test = predict(test_x, test_y, parameters)
+
+
+# L-layer Neural Network
+def L_layer_model(X, Y, layer_dims, learning_rate=0.009, num_iterations=3000, print_cost=False):
+    '''
+    Implement a L-layer neural network: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID
+
+    Arguments:
+    X -- input data, of shape (n_x, number of examples)
+    Y -- true 'label' vector (containing 0 if not-cat; 1 if cat) of shape (1, number of examples)
+    layer_dims -- list containing the input size and each layer size, of length (number of layers + 1)
+    learning_rate -- learning rate of the gradient descent update rule
+    num_iterations -- number of iterations of the optimization loop
+    print_cost -- If set to True, this will print the cost every 100 iterations
+
+    Returns:
+    parameters -- parameters learnt by model. They can be used to predict.
+    '''
+    np.random.seed(1)
+    costs = []
+
+    parameters = initialize_parameters_deep(layer_dims)
+
+    for i in range(0, num_iterations):
+        # Forward Propagation
+        AL, caches = L_model_forward(X, parameters)
+        # Compute cost
+        cost = compute_cost(AL, Y)
+        # Backward Propagation
+        grads = L_model_backward(AL, Y, caches)
+        # Update Parameters
+        parameters = update_parameters(parameters, grads, learning_rate)
+
+        if i % 100 == 0:
+            costs.append(np.squeeze(cost))
+            if print_cost:
+                print('Cost after iteration {0}: {1}'.format(i, cost))
+        
+    plt.plot(costs)
+    plt.ylabel('Cost')
+    plt.xlabel('iterations (per tens)')
+    plt.title('Learning rate=' + str(learning_rate))
+    plt.show()
+
+    return parameters
+
+layers_dims = [12288, 20, 7, 5, 1] #  5-layer model
+parameters = L_layer_model(train_x, train_y, layer_dims, num_iterations=2500, print_cost=True)
+predictions_train = predict(train_x, train_y, parameters)
+predictions_test = predict(test_x, test_y, parameters)
