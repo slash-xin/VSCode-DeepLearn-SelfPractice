d85198ad99a01e0da6b34ac692b8e6800ec9e16b
Slash.Xin <slash.xin@gmail.com>
2018-03-02 17:44:40 +0800

    update at ASUS

--- Course1-3/SelfLearning_DNNClass.py
+++ Course1-3/SelfLearning_DNNClass.py
@@ -35,7 +35,125 @@ class DnnBinaryClassifierClass:
     def InitializeParameters(self):
         '''
         '''
+        # number of layers (include the input layer).
         L = len(self.layer_dims)
+        # Initialize the matrix W and b for hidden layers and output layer
         for l in range(1, L):
             self.parameters['W' + str(l)] = np.random.randn(self.layer_dims[l], self.layer_dims[l-1]) * 0.01
             self.parameters['b' + str(l)] = np.zeros((self.layer_dims[l], 1))
+    
+    def __sigmoid(self, Z):
+        A = 1 / (1 + np.exp(-Z))
+        return A
+    def __relu(self, Z):
+        A = np.maximum(0, Z)
+        return A
+    def __tanh(self, Z):
+        A = np.tanh(Z)
+        return A
+    
+    def __activation_forward(self, A, W, b, activation):
+        '''
+        '''
+        Z = np.dot(W, A) + b
+        if activation == 'sigmoid':            
+            A = self.__sigmoid(Z)            
+        elif activation == 'relu':
+            A = self.__relu(Z)
+        elif activation == 'tanh':
+            A = self.__tanh(Z)
+        
+        cache = ((A, W, b), Z)
+        return A, cache
+
+    
+    def ForwardPropagation(self):
+        A = self.train_x
+        # number of layers in the neural network (exclude the input layer)
+        L = len(self.parameters) // 2
+        # Implement forward propagation for hidden layers
+        for l in range(1, L):
+            A_prev = A
+            A, cache = self.__activation_forward(A_prev, self.parameters['W'+str(l)], self.parameters['b'+str(l)], self.layer_activations[l-1])
+            self.caches.append(cache)
+
+        # Implement forward propagation for output layer
+        self.AL, cache = self.__activation_forward(A, self.parameters['W'+str(L)], self.parameters['b'+str(L)], 'sigmoid')
+        self.caches.append(cache)
+    
+    def ComputeCost(self):
+        '''
+        '''
+        cost = -1.0 / self.train_data_size * np.sum(np.multiply(self.train_y, np.log(self.AL)) + np.multiply((1-self.train_y), np.log(1-self.AL)), axis=1, keepdims=True)
+        cost = np.squeeze(cost)
+        return cost
+
+    def __sigmoid_backward(self, dA, cache):
+        Z = cache
+        s = 1 / (1 + np.exp(-Z))
+        dZ = dA * s * (1-s)
+        return dZ
+    def __relu_backward(self, dA, cache):
+        Z = cache
+        dZ = np.array(dA, copy=True)
+        dZ[Z <= 0] = 0
+        return dZ
+    def __tanh_backward(self, dA, cache):
+        Z = cache
+        t = np.tanh(Z)
+        dZ = 1 - np.power(t, 2)
+        return dZ
+    
+    def __activation_backward(self, dA, cache, activation):
+        linear_cache, activation_cache = cache
+
+        if activation == 'relu':
+            dZ = self.__relu_backward(dA, activation_cache)
+        elif activation == 'sigmoid':
+            dZ = self.__sigmoid_backward(dA, activation_cache)
+        elif activation == 'tanh':
+            dZ = self.__tanh_backward(dA, activation_cache)
+
+        A_prev, W, b = linear_cache
+        dW = 1.0 / self.train_data_size * np.dot(dZ, A_prev.T)
+        db = 1.0 / self.train_data_size * np.sum(dZ, axis=1, keepdims=True)
+        dA_prev = np.dot(W.T, dZ)
+
+        return dA_prev, dW, db
+    
+    def BackwardPropagation(self):
+        # number of layers (exclude input layer)
+        L = len(self.caches)
+
+        # derivative of cost function with AL
+        dAL = -(np.divide(self.train_y, self.AL) - np.divide(1 - self.train_y, 1 - self.AL))
+
+        current_cache = self.caches[L-1]
+        self.grads['dA' + str(L)], self.grads['dW'+str(L)], self.grads['db'+str(L)] = self.__activation_backward(dAL, current_cache, 'sigmoid')
+
+        for l in reversed(range(L-1)):
+            current_cache = self.caches[l]
+            self.grads['dA'+str(l+1)], self.grads['dW'+str(l+1)], self.grads['db'+str(l+1)] = self.__activation_backward(self.grads['dA'+str(l+2)], current_cache, self.layer_activations[l])
+
+    def UpdateParameters(self, learning_rate):
+        # number of layers (exclude input layer)
+        L = len(self.parameters) // 2
+        for l in range(L):
+            self.parameters['W' + str(l+1)] -= learning_rate * self.grads['dW' + str(l+1)]
+            self.parameters['b' + str(l+1)] -= learning_rate * self.grads['db' + str(l+1)]
+    
+    def TrainModel(self, learning_rate=0.0075, num_iterations=2500, print_cost=False):
+        # Initialize the parameters
+        self.InitializeParameters()
+
+        for i in range(num_iterations):
+            self.ForwardPropagation()
+            cost = self.ComputeCost()
+            self.BackwardPropagation()
+            self.UpdateParameters(learning_rate)
+
+            if i % 100 == 0:
+                self.costs.append(np.squeeze(cost))
+                if print_cost:
+                    print('Cost after iteration {0}: {1}'.format(i, cost))
+            
---
+++ Course1-3/SelfLearning_TestDNNClass.py
@@ -0,0 +1,40 @@
+
+import numpy as np
+import h5py
+from SelfLearning_DNNClass import DnnBinaryClassifierClass
+
+# Load Dataset
+def load_data(path):
+    train_dataset = h5py.File('{0}/train_catvnoncat.h5'.format(path), "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+    test_dataset = h5py.File('{0}/test_catvnoncat.h5'.format(path), "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+train_x_orig, train_y, test_x_orig, test_y, classes =load_data('course1-3/datasets')
+
+# Reshape the training and test data
+train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T
+test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+
+# Standardize data to have feature values between 0 and 1.
+train_x = train_x_flatten / 255.
+test_x = test_x_flatten / 255.
+
+
+layer_dims = (12288, 20, 7, 5, 1)
+layer_activations = ('relu', 'tanh', 'relu')
+
+model = DnnBinaryClassifierClass()
+model.InitializeTrainData(train_x, train_y)
+model.InitializeModel(layer_dims, layer_activations)
+model.TrainModel(learning_rate=0.0075, num_iterations=100, print_cost=True)
