c297664689033f679d1f0a0b42af3a5c10279b1a
Slash.Xin <slash.xin@github.com>
2018-03-12 09:20:16 +0800

    updated at MSI

--- Course2-3/SelfLearning_NNwithTF.py
+++ Course2-3/SelfLearning_NNwithTF.py
@@ -292,11 +292,11 @@ from PIL import Image
 from scipy import ndimage
 
 ## START CODE HERE ## (PUT YOUR IMAGE NAME) 
-my_image = "thumbs_up.jpg"
+my_image = "2.jpg"
 ## END CODE HERE ##
 
 # We preprocess your image to fit your algorithm.
-fname = "images/" + my_image
+fname = "Course2-3/images/" + my_image
 image = np.array(ndimage.imread(fname, flatten=False))
 my_image = scipy.misc.imresize(image, size=(64,64)).reshape((1, 64*64*3)).T
 my_image_prediction = predict(my_image, parameters)
